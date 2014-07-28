962078503601488e1e6c631291829945c6998567
0swllll9@gmail.com <0swllll9@gmail.com@a0f8c6f7-3106-7b51-00ce-2c3fcd6c2e84>
2014-07-28 06:22:09 +0000

    Login

--- trunk/wiki/web/resources/scripts/pfcrud.js
+++ trunk/wiki/web/resources/scripts/pfcrud.js
@@ -1,8 +1,19 @@
 function handleSubmit(xhr, status, args, dialog) {
-    var jqDialog = jQuery('#'+dialog.id);
-    if(args.validationFailed) {
-        jqDialog.effect('shake', { times:3 }, 100);
+    var jqDialog = jQuery('#' + dialog.id);
+    if (args.validationFailed) {
+        jqDialog.effect('shake', {times: 3}, 100);
     } else {
         dialog.hide();
+    }
+}
+
+
+function handleLoginRequest(xhr, status, args) {
+    if (args.validationFailed || !args.loggedIn) {
+        PF('dlg').jq.effect("shake", {times: 5}, 1000);
+    }
+    else {
+        PF('dlg').hide();
+        $('#loginLink').fadeOut();
     }
 }
