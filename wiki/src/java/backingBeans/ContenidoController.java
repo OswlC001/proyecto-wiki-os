ce713e0c8a7031468db0fab58ce6f6feee414e21
0swllll9 <0swllll9@gmail.com>
2014-07-28 16:44:03 +0000

    Candidato a Final

--- wiki/src/java/backingBeans/ContenidoController.java
+++ wiki/src/java/backingBeans/ContenidoController.java
@@ -2,6 +2,7 @@ package backingBeans;
 
 import entidades.Contenido;
 import entidades.Titulo;
+import entidades.Usuario;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -12,22 +13,27 @@ import javax.faces.view.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 import sessionBeans.ContenidoFacade;
+import sessionBeans.UsuarioFacade;
 
 @Named(value = "contenidoController")
 @ViewScoped
 public class ContenidoController extends AbstractController<Contenido> {
-
+    
+    @EJB
+    private UsuarioFacade usuarioFacade;
+    
     @EJB
     private ContenidoFacade contenidoFacade;
-
+    
     @Inject
     private TituloController titCodigoController;
     @Inject
     private UsuarioController usIdController;
     @Inject
     private DiscusionController discusionListController;
-
+    
     String contenido;
     String usCodigo;
     String titulo;
@@ -35,17 +41,18 @@ public class ContenidoController extends AbstractController<Contenido> {
     FacesContext facesContext = FacesContext.getCurrentInstance();
     ExternalContext externalContext = facesContext.getExternalContext();
     Map params = externalContext.getRequestParameterMap();
-
+    HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+    
     public ContenidoController() {
         // Inform the Abstract parent controller of the concrete Contenido?cap_first Entity
         super(Contenido.class);
     }
-
+    
     public String getTitulo() {
         obtDatos();
         return titulo;
     }
-
+    
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
@@ -97,36 +104,37 @@ public class ContenidoController extends AbstractController<Contenido> {
         }
         return "/discusion/index";
     }
-
+    
     public List<Contenido> getItemsContenido() {
         return contenidoFacade.getItemsContenido(titCodigoController.findObj(titCodigo));
     }
-
+    
     public void setContenido(String contenido) {
         this.contenido = contenido;
     }
-
+    
     public String getContenido() {
-        List<Contenido> listCont = contenidoFacade.getItemsContenido(titCodigoController.findObj(titCodigo));
-        contenido = "vacio";
-        if (listCont.size() > 0) {
-            Contenido contObj = listCont.get(listCont.size() - 1);
-            if (contObj != null) {
-                contenido = contObj.getConContenido();
+        try {
+            List<Contenido> listCont = contenidoFacade.getItemsContenido(titCodigoController.findObj(titCodigo));
+            contenido = "vacio";
+            if (listCont.size() > 0) {
+                Contenido contObj = listCont.get(listCont.size() - 1);
+                if (contObj != null) {
+                    contenido = contObj.getConContenido();
+                }
             }
+        } catch (Exception ex) {
         }
         return contenido;
     }
-
+    
     public Integer getTitCodigo() {
         return titCodigo;
     }
-
+    
     public void setTitCodigo(Integer titCodigo) {
         this.titCodigo = titCodigo;
     }
-
-    
     
     public String guardar() {
         Contenido contObj = new Contenido();
@@ -137,23 +145,50 @@ public class ContenidoController extends AbstractController<Contenido> {
         contenidoFacade.create(contObj);
         return "/contenido/index.xhtml";
     }
-
+    
+    public String getUsCodigo() {
+        return usCodigo;
+    }
+    
+    public void setUsCodigo(String usCodigo) {
+        this.usCodigo = usCodigo;
+    }
+    
     private void obtDatos() {
-        usCodigo = "Oswaldo";        
-        Object param = params.get("param");
-        if (param == null) {
-            if (titCodigo == null) {
-                titCodigo = 1;
+        try {
+            usCodigo = (String) session.getAttribute("usuario");
+            Object param = params.get("param");
+            if (param == null) {
+                if (titCodigo == null) {
+                    titCodigo = 1;
+                }
+            } else {
+                titCodigo = new Integer(param.toString());
             }
-        } else {
-            titCodigo = new Integer(param.toString());
-        }
-        Titulo titObj = titCodigoController.findObj(titCodigo);
-        if (titObj != null) {
-            titulo = titObj.getTitTitulo();
-        } else {
-            titulo = "";
+            Titulo titObj = titCodigoController.findObj(titCodigo);
+            if (titObj != null) {
+                titulo = titObj.getTitTitulo();
+            } else {
+                titulo = "";
+            }
+        } catch (Exception ex) {
         }
     }
-
+    
+    public List<Usuario> getItemsUsuario() {
+        usCodigo = (String) session.getAttribute("usuario");
+        List<Usuario> itemsUsuario = null;
+        if (usCodigo != null) {
+            itemsUsuario = usuarioFacade.findByID(usCodigo);
+        }
+        return itemsUsuario;
+    }
+    
+    public void actualizarUsuario() {
+        usCodigo = usIdController.getSelected().getUsId();
+        if (usCodigo != null) {
+            session.setAttribute("usuario", usCodigo);
+        }
+    }
+    
 }
--- wiki/src/java/backingBeans/UserLoginView.java
+++ wiki/src/java/backingBeans/UserLoginView.java
@@ -9,28 +9,42 @@ package backingBeans;
  *
  * @author SiperProg
  */
+import entidades.Usuario;
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.servlet.http.HttpSession;
 
 import org.primefaces.context.RequestContext;
+import sessionBeans.UsuarioFacade;
 
 @ManagedBean
 public class UserLoginView {
 
+    @EJB
+    private UsuarioFacade usuarioFacade;
+
     private String username;
     private String password;
-    private String ingreso = "Ingresar";
+    private String ingreso;
+    HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 
     public String getIngreso() {
+        Boolean logeado = (Boolean) session.getAttribute("sesionLoggedIn");
+        if (logeado != null && logeado) {
+            String usuario = (String) session.getAttribute("usuario");
+            ingreso = usuario + " (Cuenta)";
+        } else {
+            ingreso = "Ingresar";
+        }
         return ingreso;
     }
 
     public void setIngreso(String ingreso) {
         this.ingreso = ingreso;
-    }   
-    
+    }
 
     public String getUsername() {
         return username;
@@ -52,22 +66,32 @@ public class UserLoginView {
         RequestContext context = RequestContext.getCurrentInstance();
         FacesMessage message = null;
         boolean loggedIn = false;
-
-        if (username != null && username.equals("admin") && password != null && password.equals("admin")) {
-            loggedIn = true;
-            ingreso = username + "(Salir)";
-            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+        Usuario usuario = usuarioFacade.find(username);
+        if (usuario != null) {
+            if (username != null && username.equals(usuario.getUsId()) && password != null && password.equals(usuario.getUsClave())) {
+                loggedIn = true;
+                ingreso = "Bienvenido " + username;
+                context.addCallbackParam("usuario", username);
+                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+            } else {
+                loggedIn = false;
+                message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
+            }
         } else {
             loggedIn = false;
-            ingreso = "Ingresar";
             message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
         }
 
         FacesContext.getCurrentInstance().addMessage(null, message);
         context.addCallbackParam("loggedIn", loggedIn);
+        session.setAttribute("sesionLoggedIn", loggedIn);
+        session.setAttribute("usuario", username);
     }
-    
-    public void logout(){
-        
+
+    public void logout() {
+        session.setAttribute("sesionLoggedIn", false);
+        session.setAttribute("usuario", null);
+        username = null;
+        password = null;
     }
 }
